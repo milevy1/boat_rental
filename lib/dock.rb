105a6e836f6f7f79fb6fa9cc2f1996110cc06eba
Matt Levy <milevy1@gmail.com>
2019-01-27 15:49:02 -0700

    Added dock charge method

--- lib/dock.rb
+++ lib/dock.rb
@@ -11,4 +11,12 @@ class Dock
     @rental_log[boat] = renter
   end
 
+  def charge(boat)
+    result = {}
+    renter = @rental_log[boat]
+    result[:card_number] = renter.credit_card_number
+    result[:amount] = boat.price_per_hour * boat.hours_rented
+    return result
+  end
+
 end
